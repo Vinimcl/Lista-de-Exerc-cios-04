97d3acc2f9ea5cfa4f82d114ba2b567298574ebc
juBorba <167782530+juBorba@users.noreply.github.com>
2024-05-13 16:15:11 -0300

    Update Programa.java

--- cenario2/src/br/edu/up/Programa.java
+++ cenario2/src/br/edu/up/Programa.java
@@ -1,43 +1,8 @@
-import java.lang.Math;
+package br.edu.up;
 
-class Ponto {
-    private double x;
-    private double y;
-
-    public Ponto() {
-        this.x = 0;
-        this.y = 0;
-    }
-
-    public Ponto(double x, double y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public double calcularDistancia(Ponto outroPonto) {
-        double distancia = Math
-                .sqrt(Math.pow((outroPonto.getX() - this.x), 2) + Math.pow((outroPonto.getY() - this.y), 2));
-        return distancia;
-    }
-
-    public double getX() {
-        return x;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
-}
-
+import br.edu.up.modelos.Ponto;
 public class Programa {
+
     public static void main(String[] args) {
         Ponto ponto1 = new Ponto();
 
