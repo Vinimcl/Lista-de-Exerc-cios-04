7ce20fb319f4f222ad9a94eb2c85824d803dd1fa
juBorba <167782530+juBorba@users.noreply.github.com>
2024-05-13 16:12:46 -0300

    Create Agenda.java

---
+++ cenario3/src/modelos/Agenda.java
@@ -0,0 +1,18 @@
+class Agenda {
+    private Map<String, List<Compromisso>> compromissos;
+
+    public Agenda() {
+        compromissos = new HashMap<>();
+    }
+
+    public void adicionarCompromisso(String data, Compromisso compromisso) {
+        if (!compromissos.containsKey(data)) {
+            compromissos.put(data, new ArrayList<>());
+        }
+        compromissos.get(data).add(compromisso);
+    }
+
+    public List<Compromisso> getCompromissos(String data) {
+        return compromissos.getOrDefault(data, new ArrayList<>());
+    }
+}
