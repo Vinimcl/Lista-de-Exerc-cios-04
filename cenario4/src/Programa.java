85d62381c2f807421a06027b2abb682d0e335863
Juliana <jusborba.facul@gmail.com>
2024-05-13 21:23:35 -0300

    Update Programa.java

--- cenario4/src/Programa.java
+++ cenario4/src/Programa.java
@@ -1,44 +1,37 @@
+import telas.EstacionamentoView;;
+
 public class Programa {
     public static void main(String[] args) {
         EstacionamentoView view = new EstacionamentoView();
-        EstacionamentoController controller = new EstacionamentoController(10, 5);
 
-        int opcao;
+        String opcaoStr;
         do {
             view.exibirMenu();
             System.out.print("Escolha uma opção: ");
-            opcao = Integer.parseInt(view.obterPlaca());
-            switch (opcao) {
-                case 1:
-                    Veiculo novoVeiculo = view.obterDadosVeiculo();
-                    if (controller.entradaVeiculo(novoVeiculo)) {
-                        view.exibirMensagem("Veículo estacionado com sucesso!");
-                    } else {
-                        view.exibirMensagem("Estacionamento cheio!");
-                    }
+            opcaoStr = view.obterPlaca(); // Obter a opção como uma String
+            try {
+
+            } catch (NumberFormatException e) {
+
+            }
+
+            switch (opcaoStr) { // Comparar como uma String
+                case "1": // Opção 1
+                    // Código para opção 1
                     break;
-                case 2:
-                    String placa = view.obterPlaca();
-                    Veiculo veiculoSaindo = controller.saidaVeiculo(placa);
-                    if (veiculoSaindo != null) {
-                        view.exibirMensagem("Veículo de placa " + placa + " saiu do estacionamento.");
-                    } else {
-                        view.exibirMensagem("Veículo não encontrado no estacionamento.");
-                    }
+                case "2": // Opção 2
+                    // Código para opção 2
                     break;
-                case 3:
-                    int entradas = controller.getNumeroVeiculos();
-                    int saidas = 10 - entradas;
-                    int valor = controller.calcularPagamento();
-                    view.exibirRelatorio(entradas, saidas, valor);
+                case "3": // Opção 3
+                    // Código para opção 3
                     break;
-                case 4:
-                    view.exibirMensagem("Saindo...");
+                case "4": // Opção 4
+                    // Código para opção 4
                     break;
                 default:
                     view.exibirMensagem("Opção inválida!");
             }
-        } while (opcao != 4);
+        } while (!opcaoStr.equals("4")); // Verificar a opção como uma String
 
         view.fecharScanner();
     }
